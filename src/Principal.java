40c8bd9958fe0566f05b97f05ee86f54ffc4fa76
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2019-10-02 08:42:55 -0300

    ajuste do arquivo

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,7 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -13,15 +15,55 @@ public class Principal {
         Scanner in;
         String[] stringdaLinha;
         int col=0;
+        int contador=0;
+        List<Processo> processos= new ArrayList<>();
         try {
             in = new Scanner(new FileReader(file));
             while (in.hasNextLine()) {
                 col++;
                 String line = in.nextLine();
+                String nome = "";
+                String op = "";
+                String valor = "";
                 stringdaLinha =  line.split(" ");
-                for(int i =0 ;i<stringdaLinha.length;i++)
-                    if(!stringdaLinha[i].equals(""))
-                        System.out.println(stringdaLinha[i]);
+                for(int i =0 ;i<stringdaLinha.length;i++) {
+
+                    if (!stringdaLinha[i].equals("")) {
+                        contador++;
+                        if(contador%3==1) {
+//                            System.out.println(stringdaLinha[i]);
+                            nome = stringdaLinha[i];
+                        }
+                        else if(contador%3==2){
+                            op = stringdaLinha[i];
+                        }else{
+                            valor = stringdaLinha[i];
+                        }
+                    }
+                }
+                int count = 0;
+                for(int i =0 ;i<stringdaLinha.length;i++) {
+                    if (op.equals("C") && count==0) {
+                        System.out.println("criando processo " + nome);
+                        Processo processo = new Processo();
+                        processo.setNome(nome);
+                        processo.setTamanho(Integer.parseInt(valor));
+                        processos.add(processo);
+                        count++;
+                        if(count==3)
+                            count=0;
+                    } else if (op.equals("I/O")&& count==0) {
+                        System.out.println("Processo " + nome + " bloqueado por operação de entrada e saída");
+                        count++;
+                        if(count==3)
+                            count=0;
+                    } else if ((op.equals("W") || op.equals("R"))&& count==0) {
+                        System.out.println("alocando processo " + nome + " no endereço " + valor + " na operação de " + op);
+                        count++;
+                        if(count==3)
+                            count=0;
+                    }
+                }
 //                System.out.println(stringdaLinha[col*1]);
 //                System.out.println(stringdaLinha[col*2]);
 //                System.out.println(stringdaLinha[col*3]);
