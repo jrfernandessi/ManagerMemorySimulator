c123b2dbf09ba3f7fbc07bb3c591230e4b08cb22
Junior Fernandes <jrfernandessi@users.noreply.github.com>
2019-10-03 14:04:46 -0300

    add structs of simulator

---
+++ src/Frame.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Frame {
+    private List<Integer> instructionList = new ArrayList<>();
+    private int size;
+    private int address;
+    private Process process;
+
+    public Frame(List<Integer> instructionList, int size, int address, Process process) {
+        this.instructionList = instructionList;
+        this.size = size;
+        this.address = address;
+        this.process = process;
+    }
+
+    public List<Integer> getInstructionList() {
+        return instructionList;
+    }
+
+    public void setInstructionList(List<Integer> instructionList) {
+        this.instructionList = instructionList;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getAddress() {
+        return address;
+    }
+
+    public void setAddress(int address) {
+        this.address = address;
+    }
+
+    public Process getProcess() {
+        return process;
+    }
+
+    public void setProcess(Process process) {
+        this.process = process;
+    }
+}
---
+++ src/Process.java
@@ -0,0 +1,34 @@
+public class Process {
+    private int size;
+    private String name;
+    private int count;
+
+    Process(){
+        count=0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+
+}
